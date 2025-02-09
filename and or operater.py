2aa986ce5a3243ec8efbfe1fda22c3bb1aa28bfd
nehanapkhan <nehanapkhan2k@gmail.com>
2025-02-09 11:05:57 -0500

    Add files via upload

---
+++ and or operater.py
@@ -0,0 +1,20 @@
+a=10
+b=12
+c=0
+
+if a and b and c:
+      print("all the number have bolean value as true")
+else:
+      print("atleast one number has bollean value as flase")
+
+      a= 10
+b=-10
+c=0
+if a>0 or b>0:
+    print("either of the number is greather than 0")
+else:
+    print("no number is greater than 0")
+    if b>0 or c>0:
+        primt("either of the number is greather than 0")
+    else:
+        print("no number is greather than 0")
