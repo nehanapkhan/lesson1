9787ab171661d4820bc28edff9b84e6c4fc6d080
e <nehanapkhan2k@gmail.com>
2025-03-01 11:02:17 -0500

    Add files via upload

---
+++ right angle triangle .py
@@ -0,0 +1,9 @@
+print("half pyramid of stars")
+n = int(input("enter the number of rows:"))
+#outer loop to handle rowss
+for i in range(n):
+    #inner loop to handle columns
+    for j in range(1+1):
+        print("x",end="")
+
+        print()  
